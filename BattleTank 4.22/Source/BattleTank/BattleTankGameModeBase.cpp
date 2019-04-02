bcc6a318482a4fdaccacd5e798524147e0bbebb5
TarikNeaj <Tarik_lg3@hotmail.com>
2019-04-02 19:51:03 +0200

    Upgraded to Unreal Engine Version 4.22 & Added Main Menu Screen

---
+++ BattleTank 4.22/Source/BattleTank/BattleTankGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTankGameModeBase.h"
+
+void ABattleTankGameModeBase::YourMomGay()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		int x = 222;
+	}
+}
